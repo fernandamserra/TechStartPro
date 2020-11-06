c2c15895dd0d77b75c84d620fe8297a98173ac63
fmserrat <fmserrat@gmail.com>
2020-11-05 23:13:41 -0400

    added cors so I can make http requests and added acess controllers in the header.

--- crud/backend/src/app.js
+++ crud/backend/src/app.js
@@ -1,9 +1,18 @@
+const express = require('express');
+
 const bodyParser = require('body-parser');
 
-const express = require('express');
-//const cors = require('cors')
+const cors = require('cors')
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Meethods", "GET, PUT, POST, DELETE, OPTIONS"); // update to match the domain you will make the request from
+  next();
+});
 
 const routes = require('./routes');
 
@@ -11,6 +20,6 @@ app.use(bodyParser.json());
 
 app.use(routes);
 
-//app.use(cors());
+app.use(cors());
 
 module.exports = app;
