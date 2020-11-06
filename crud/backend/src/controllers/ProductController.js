9d0d2c930d017490e879eabe8c2f7f7a6a2ed702
fmserrat <fmserrat@gmail.com>
2020-11-06 02:41:44 -0400

    refactored delete method from product controller.

--- crud/backend/src/controllers/ProductController.js
+++ crud/backend/src/controllers/ProductController.js
@@ -14,16 +14,14 @@ module.exports =
             description,
             value
         }).returning('*');
-        console.log(category);
+
         const categories_to_create = category.map((cat_id)=>(
             {
                 prod_id:response.id,
                 cat_id
             }
         ));
-        console.log(categories_to_create);
         const response2 = await connection('product_categories').insert(categories_to_create);
-        console.log(response);
         return resp.json(response);
     },
 
@@ -54,10 +52,10 @@ module.exports =
         {
             if(command != "") 
             {
-                command = command + `OR product.value = '%${value}%' `;
+                command = command + `OR product.value = '${value}' `;
             }else
             {
-                command =  `WHERE product.value = '%${value}%' `;
+                command =  `WHERE product.value = '${value}' `;
             }
         }
 
@@ -79,7 +77,6 @@ module.exports =
                 command = command + `WHERE categories.name in (${list}) `;
                 //command = command + `WHERE categories.name in (${categories.map((element)=>(`\'${element}\'`))}) `;
             }
-            console.log(categories, list);
         
         }
         const products = await connection.raw(`select STRING_AGG(categories.name, ',' ORDER BY categories.id) as prod_cat,
@@ -93,28 +90,13 @@ module.exports =
             
         resp.header('X-Total-Count', count['count(*)']);
 
-        /*
-        const products = await connection('product')
-            .leftJoin('product_categories', 'product.id', '=', 'product_categories.prod_id')
-            .leftJoin('categories', 'categories.id', '=', 'product_categories.cat_id')
-            .groupBy('product.id')
-            //.limit(20)
-            //.offset((page-1)*20)
-            .select(["product.id",
-                    "product.name as prod_name", 
-                    "product.description",
-                    "product.value",
-                    "categories.name as cat_name",
-                    connection.raw('STRING_AGG(categories.name, ',' ORDER BY categories.id)')]);        
-
-            resp.header('X-Total-Count', count['count(*)']);
-*/
         return resp.json({products:products.rows});
     },
 
     async delete(req, resp)
     {
         const {id} = req.params;
+        await connection('product_categories').where('prod_id', id).delete();
         await connection('product').where('id',id).delete();
         
         // Cód. 204: No Content - resposta de sucesso sem corpo
